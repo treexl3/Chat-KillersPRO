0cbcf9d09068516f36136eb45d199511a07029b5
treexl <115030662+treexl3@users.noreply.github.com>
2025-04-08 20:37:17 +0300

    change

--- index.js
+++ index.js
@@ -54,22 +54,19 @@ io.on('connection', socket => {
       <span class="time">[${formatTime()}]</span> 
       <span class="text">Welcome to the room: ${room}</span>
     </div>`;
-    
-    socket.once("details", (({ }) => {
-      // Format system messages with timestamp
-      const systemMessage = `<div class="system-message">
-        <span class="time">[${formatTime()}]</span> 
-        <span class="text">${username} has joined the room.</span>
-      </div>`;
-
-      socket.to(room).emit('message', systemMessage);
-    }))
 
     socket.emit('message', welcomeMessage);
 
     io.to(room).emit('roomUsers', getUsersInRoom(room));
   });
-  socket.once("details", (({ username, room }) => {
+  socket.once("details", (({ }) => {
+    // Format system messages with timestamp
+    const systemMessage = `<div class="system-message">
+      <span class="time">[${formatTime()}]</span> 
+      <span class="text">${username} has joined the room.</span>
+    </div>`;
+
+    socket.to(room).emit('message', systemMessage);
   }));
   socket.on('chatMessage', msg => {
     const user = users[socket.id];
