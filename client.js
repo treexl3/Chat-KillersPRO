0c54481cf335827cf70935266470703bacef69aa
treexl <115030662+treexl3@users.noreply.github.com>
2025-04-08 17:46:55 +0300

    first commit

---
+++ client.js
@@ -0,0 +1,28 @@
+const socket = io();
+const urlParams = new URLSearchParams(window.location.search);
+const username = urlParams.get('username');
+const room = urlParams.get('room');
+
+if (username && room) {
+  socket.emit('joinRoom', { username, room });
+  document.getElementById('room-name').innerText = `Room: ${room}`;
+}
+
+socket.on('message', message => {
+  const div = document.createElement('div');
+  div.textContent = message;
+  document.getElementById('messages').appendChild(div);
+});
+
+socket.on('roomUsers', users => {
+  const userList = document.getElementById('user-list');
+  userList.innerHTML = `<strong>Users in room:</strong><br>${users.join('<br>')}`;
+});
+
+document.getElementById('message-form').addEventListener('submit', e => {
+  e.preventDefault();
+  const msgInput = document.getElementById('msg');
+  const msg = msgInput.value;
+  socket.emit('chatMessage', msg);
+  msgInput.value = '';
+});
