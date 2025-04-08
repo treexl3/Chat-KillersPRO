b3716a13d64228e3a27e00164672a210d28cce90
treexl <115030662+treexl3@users.noreply.github.com>
2025-04-08 22:07:30 +0300

    change

--- client.js
+++ client.js
@@ -3,6 +3,21 @@ const urlParams = new URLSearchParams(window.location.search);
 const username = urlParams.get('username');
 const room = urlParams.get('room');
 
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+const usernames = document.querySelectorAll('.username');
+usernames.forEach((el, index) => {
+  // Use getRandomColor() or getHSLColor()
+  el.style.color = getHSLColor(index, usernames.length);
+});
+
 // Function to display messages
 function displayMessage(message) {
   const div = document.createElement('div');
